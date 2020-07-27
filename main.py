f9bd7616d799ba3ffc97dc4c537b783affdd7f0a
evgeniy gazetdinov <zplacesound@gmail.com>
2020-07-27 19:49:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,45 @@ from variables import variable
 import vk as VKONTAKTE
 import time 
 from datetime import datetime
+from datetime import timedelta
 from collections import OrderedDict
+import csv
+import os
+import re
 
-
-
-word_for_search = [
-    'сайт','сайт под ключ','дизайн'
+words = [
+    'веб','web','сайт','сайт под ключ','дизайн','Tilda','tilda','php','python','django','wordpress','wp','веб-дизайн'
 ]
+
+class DictUnicodeWriter(object):
+
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, D):
+        self.writer.writerow({k:v.encode("utf-8") for k,v in D.items()})
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for D in rows:
+            self.writerow(D)
+
+    def writeheader(self):
+        self.writer.writeheader()
+
+
 
 
 def get_token():
@@ -27,25 +59,49 @@ def parse_wall(groupid):
     # С каждым проходом цикла смещение offset увеличивается на тысячу
     # и еще тысяча id'шников добавляется к нашему списку.
     for i in range(1, count+1):
-        time.sleep(0.2) 
+        time.sleep(0.3) 
         data = data + vk_api.wall.get(owner_id='-40527789',v=5.92,offset=i*1000)["items"]
     return data
 
 
 
 
-def get_text_and_date(data):
+def get_text_and_date(data,words):
     res = []
     count= 0
+    date= datetime
+    yesterday = date.today() - timedelta(days=1)
     for d in data:
-        res.append({'id':count+1,'text': d['text'],'date':datetime.fromtimestamp(d['date']).strftime("%d, %Y %I:%M:%S")})
-    return res
+        count+=1
+        date_d = datetime.fromtimestamp(d['date'])
+        if date_d < yesterday:
+            continue
+        for word in words:
+            if re.match(r'{}'.format(word), d['text']):
+                res.append({'id':(count),'text': d['text'],'date':datetime.fromtimestamp(d['date']).strftime('%Y-%m-%d %H:%M:%S')})
+                
+            else: 
+                continue
+
+    return sorted(res, key = lambda i: i['date'],reverse=True)
+
+def write_to_csv(info_for_write):
+#notice use OrderedDict for save order
+    fields = ['id','text','date']
+    file = 'out.csv'
+    with open(file,'a+',encoding='utf-8-sig',newline='') as f:
+        csv_dict = [row for row in csv.DictReader(f)]
+        w = csv.DictWriter(f,fieldnames=fields)
+        if os.stat(file).st_size == 0:
+            w.writeheader()
+        for r in info_for_write:    
+            w.writerow(r)
 
 
 
 def find_sites_in_data(data):
-    cleaned_data = get_text_and_date(data)
-    print(cleaned_data)
+    cleaned_data = get_text_and_date(data,words)
+    write_to_csv(cleaned_data)
 
 
 token = get_token()
